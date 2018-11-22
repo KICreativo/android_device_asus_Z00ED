bf6dc33002d2cc9c2d8631b67308ae1af6a54d86
KICreativo <kicreativos@gmail.com>
2018-11-22 11:13:41 +0300

    changes kernel defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_OTA_ASSERT_DEVICE := Z00ED
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Kernel
-TARGET_KERNEL_CONFIG := Z00ED-custom_defconfig
+TARGET_KERNEL_CONFIG := ze500kl-custom_defconfig
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
